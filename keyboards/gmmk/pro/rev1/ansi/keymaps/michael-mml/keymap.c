74925d0c797a674c97d7a2b2f040bf5e5fa8c541
michael-mml <mml.liu@mail.utoronto.ca>
2024-06-23 21:26:03 -0700

    Remove OSM for modifiers because of interference with gaming

--- keyboards/gmmk/pro/rev1/ansi/keymaps/michael-mml/keymap.c
+++ keyboards/gmmk/pro/rev1/ansi/keymaps/michael-mml/keymap.c
@@ -77,21 +77,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // https://docs.qmk.fm/#/one_shot_keys?id=one-shot-keys for examples.
     // clang-format off
     [WIN] = LAYOUT(
-        KC_ESC,        KC_F1,         KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,        KC_F11,      KC_F12,        KC_PSCR,                KC_MUTE,
-        KC_GRV,        KC_1,          KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,          KC_MINS,     KC_EQL,        KC_BSPC,                KC_INS,
-        KC_TAB,        KC_Q,          KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,          KC_LBRC,     KC_RBRC,       KC_BSLS,                KC_DEL,
-        KC_CAPS,       KC_A,          KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,       KC_QUOT,                    KC_ENT,                 KC_HOME,
-        OSM(MOD_LSFT),                KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,        KC_SLSH,                    OSM(MOD_RSFT), KC_UP,   KC_END,
-        OSM(MOD_LCTL), OSM(MOD_LGUI), OSM(MOD_LALT),                      KC_SPC,                             OSM(MOD_RALT), OSL(WIN_FN), OSM(MOD_RCTL), KC_LEFT,       KC_DOWN, KC_RGHT
+        KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,      KC_F11,   KC_F12,  KC_PSCR,          KC_MUTE,
+        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,        KC_MINS,  KC_EQL,  KC_BSPC,          KC_INS,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,        KC_LBRC,  KC_RBRC, KC_BSLS,          KC_DEL,
+        KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,     KC_QUOT,           KC_ENT,           KC_HOME,
+        KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,      KC_SLSH,           KC_RSFT, KC_UP,   KC_END,
+        KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                    KC_RALT, OSL(WIN_FN),           KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
     ),
 
     [WIN_FN] = LAYOUT(
-        _______,       KC_MYCM,       KC_WHOM, KC_CALC, KC_MSEL, KC_MPRV, KC_MNXT, KC_MPLY, KC_MSTP, KC_MUTE, KC_VOLD,       KC_VOLU,     _______,       _______,                RGB_TOG,
-        _______,       _______,       DF(MAC), _______, _______, _______, _______, KC_NUM,  KC_PEQL, KC_PSLS, KC_PAST,       RGB_RMOD,    RGB_MOD,       _______,                KC_BTN1,
-        _______,       _______,       RGB_HUI, _______, _______, _______, _______, KC_P7,   KC_P8,   KC_P9,   KC_PMNS,       _______,     _______,       QK_BOOT,                KC_BTN2,
-        _______,       RGB_SPD,       RGB_HUD, RGB_SPI, _______, _______, _______, KC_P4,   KC_P5,   KC_P6,   KC_PPLS,       _______,                    _______,                KC_PGUP,
-        _______,                      _______, _______, _______, _______, NK_TOGG, _______, KC_P1  , KC_P2,   KC_P3,         KC_PDOT,                    _______,       KC_MS_U, KC_PGDN,
-        _______,       _______,       _______,                            _______,                            KC_P0,         _______,     _______,       KC_MS_L,       KC_MS_D, KC_MS_R
+        _______, KC_MYCM, KC_WHOM, KC_CALC, KC_MSEL, KC_MPRV, KC_MNXT, KC_MPLY, KC_MSTP, KC_MUTE, KC_VOLD,     KC_VOLU,  _______, _______,          RGB_TOG,
+        _______, _______, DF(MAC), _______, _______, _______, _______, KC_NUM,  KC_PEQL, KC_PSLS, KC_PAST,     RGB_RMOD, RGB_MOD, _______,          KC_BTN1,
+        _______, _______, RGB_HUI, _______, _______, _______, _______, KC_P7,   KC_P8,   KC_P9,   KC_PMNS,     _______,  _______, QK_BOOT,          KC_BTN2,
+        _______, RGB_SPD, RGB_HUD, RGB_SPI, _______, _______, _______, KC_P4,   KC_P5,   KC_P6,   KC_PPLS,     _______,           _______,          KC_PGUP,
+        _______,          _______, _______, _______, _______, NK_TOGG, _______, KC_P1,   KC_P2,   KC_P3,       KC_PDOT,           _______, KC_MS_U, KC_PGDN,
+        _______, _______, _______,                            _______,                            KC_P0,       _______,  _______, KC_MS_L, KC_MS_D, KC_MS_R
     ),
 
     [MAC] = LAYOUT(
